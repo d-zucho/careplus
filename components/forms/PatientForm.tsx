d323f8718b87a4830260ec8d189510559ba22f78
D_Zucho <danny.nico.dev@outlook.com>
2024-09-06 00:38:40 -0700

    add home hero image and starting form

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -1,7 +1,72 @@
-import React from 'react'
+'use client'
+
+// form
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+// validation
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { Input } from '../ui/input'
+import { Button } from '../ui/button'
+
+const formSchema = z.object({
+  username: z.string().min(5, {
+    message: 'Username must be at least 5 characters.',
+  }),
+})
 
 const PatientForm = () => {
-  return <div>PatientForm</div>
+  // 1. Define your form
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    // a resolver is a function that takes a form schema and returns a form resolver which is a function that returns a form with validation meaning that it will validate the form schema
+    defaultValues: {
+      username: '',
+    },
+  })
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-6 flex-1'>
+        <section className='mb-12 space-y-4'>
+          <h1 className='header'>Hi there 👋</h1>
+          <p className='text-dark-700'>Schedule your first appointment</p>
+        </section>
+        <FormField
+          control={form.control}
+          name='username'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder='shadcn' {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type='submit'>Submit</Button>
+      </form>
+    </Form>
+  )
 }
 
 export default PatientForm
