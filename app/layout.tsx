b876ea1c2e7a7d946378c366642b5c10cad6d2b3
D_Zucho <danny.nico.dev@outlook.com>
2024-09-05 21:00:36 -0700

    add basic skelaton of homepage w/o form

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Plus_Jakarta_Sans } from 'next/font/google'
 import './globals.css'
 import { cn } from '@/lib/utils'
+import { ThemeProvider } from '@/components/theme-provider'
 
 const fontSans = Plus_Jakarta_Sans({
   subsets: ['latin'],
@@ -28,7 +29,9 @@ export default function RootLayout({
             fontSans.variable
           )}
         >
-          Home
+          <ThemeProvider attribute='class' defaultTheme='dark'>
+            {children}
+          </ThemeProvider>
         </div>
       </body>
     </html>
